4d69f19f5f92a572a3db151be03dab5f2586023b
clemabro <clementazibeiro@sfr.fr>
2020-07-30 17:09:00 +0200

    Modif de la vue

--- src/main/java/vue/Vue.java
+++ src/main/java/vue/Vue.java
@@ -1,12 +1,16 @@
 package vue;
 
+import bean.User;
+import dao.DAOUser;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
@@ -17,43 +21,104 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 public class Vue extends Application {
+    BorderPane paneConnexion = new BorderPane();
+    GridPane gridConnexion = new GridPane();
+    Text scenetitle = new Text("Bienvenue");
+    Label userName = new Label("Nom d'utilisateur:");
+    TextField userTextField = new TextField();
+    Label pw = new Label("Mot de passe :");
+    PasswordField pwBox = new PasswordField();
+    Button btnSign = new Button("Se connecter");
+    HBox hbBtn = new HBox(10);
+    final Text actiontarget = new Text();
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
+        gridConnexion.add(scenetitle, 0, 0, 2, 1);
+        gridConnexion.add(userName, 0, 1);
+        gridConnexion.add(userTextField, 1, 1);
+        gridConnexion.add(pw, 0, 2);
+        gridConnexion.add(pwBox, 1, 2);
+        gridConnexion.add(hbBtn, 1, 4);
+        gridConnexion.add(actiontarget, 1, 6);
+        gridConnexion.setAlignment(Pos.CENTER);
+        gridConnexion.setHgap(10);
+        gridConnexion.setVgap(10);
+        gridConnexion.setPadding(new Insets(25, 25, 25, 25));
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btnSign);
 
-        BorderPane root = new BorderPane();
-        HBox hbox = new HBox();
-        VBox vbox = new VBox();
+        paneConnexion.setCenter(gridConnexion);
+        Scene sceneConnexion = new Scene(paneConnexion);
+        primaryStage.setScene(sceneConnexion);
+        primaryStage.show();
 
-        primaryStage.setTitle("Hello World!");
-        // ------ Label 1 -----
-        Text text = new Text("test text");
-        text.setFont(Font.font ("Verdana", 20));
-        text.setFill(Color.RED);
+        TableView<User> table = new TableView<User>();
 
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        // Create column UserName (Data type of String).
+        TableColumn<User, String> userNameCol //
+                = new TableColumn<User, String>("User Name");
 
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        // Create column Email (Data type of String).
+        TableColumn<User, String> emailCol//
+                = new TableColumn<User, String>("Email");
+
+        // Create column FullName (Data type of String).
+        TableColumn<User, String> fullNameCol//
+                = new TableColumn<User, String>("Full Name");
+
+        // Create 2 sub column for FullName.
+        TableColumn<User, String> firstNameCol//
+                = new TableColumn<User, String>("First Name");
+
+        TableColumn<User, String> lastNameCol //
+                = new TableColumn<User, String>("Last Name");
+
+        // Add sub columns to the FullName
+        fullNameCol.getColumns().addAll(firstNameCol, lastNameCol);
+
+        // Active Column
+        TableColumn<User, Boolean> activeCol//
+                = new TableColumn<User, Boolean>("Active");
+
+        // Defines how to fill data for each cell.
+        // Get value from property of UserAccount. .
+        userNameCol.setCellValueFactory(new PropertyValueFactory<User, String>("login"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<User, String>("nom"));
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<User, String>("prenom"));
+        activeCol.setCellValueFactory(new PropertyValueFactory<User, Boolean>("active"));
+
+        // Set Sort type for userName column
+        userNameCol.setSortType(TableColumn.SortType.DESCENDING);
+        lastNameCol.setSortable(false);
+
+        // Display row data
+        ObservableList<User> list = FXCollections.observableArrayList(DAOUser.getInstance().getAll());
+        table.setItems(list);
+
+        table.getColumns().addAll(userNameCol, emailCol, fullNameCol, activeCol);
+
+        StackPane root = new StackPane();
+        root.setPadding(new Insets(5));
+        root.getChildren().add(table);
+
+        final Scene sceneTable = new Scene(root, 450, 300);
+
+        // action lors de lappuie sur le btn sign in
+        btnSign.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setText("Sign in button pressed");
+                primaryStage.setScene(sceneTable);
             }
         });
-
-
-        vbox.getChildren().add(btn);
-        vbox.getChildren().add(text);
-
-        root.setTop(vbox);
-        Scene scene=new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
